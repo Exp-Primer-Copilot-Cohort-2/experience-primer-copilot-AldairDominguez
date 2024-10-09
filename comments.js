e15a68f2a518cecf1127eb7cb61baa81ffd824c4
Aldair Dominguez <89713632+AldairDominguez@users.noreply.github.com>
2024-10-09 07:12:55 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,6 @@
+// Create web server
+// Create a web server that listens to the port 3000. It should respond to the following URLs:
+// /comments - returns a list of comments from the file comments.json
+// /comment - adds a comment to the comments.json file
+// /count - returns the number of comments in the comments.json file
+// The server should read and write to the comments.json file using the fs module. If the comments.json file does not exist, it should be created with an empty array.
